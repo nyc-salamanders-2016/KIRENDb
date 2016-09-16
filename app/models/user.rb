1ad7680e71d0bacd524124249e87d8d3a01d0f5b
Kiren Srinivasan <email": null>
2016-09-16 10:48:42 -0400

    Adds User model; incorporates rspec into environment

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :username,        { presence: true, uniqueness: true }
+  validates :password,        { confirmation: true }
+  validates :password_digest, { presence: true }
+
+  has_secure_password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915165319) do
+ActiveRecord::Schema.define(version: 20160916143830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160915165319) do
     t.boolean  "is_previous"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
